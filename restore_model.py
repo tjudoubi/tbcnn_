290087b86ba576890d682702e1ac8bbeb14f7ead
tjudoubi <2624593014@qq.com>
2020-08-10 12:18:47 +0800

    add interface

--- restore_model.py
+++ restore_model.py
@@ -5,19 +5,18 @@ import network3
 from sampleJS import get_tree
 from sampleJS import getData_finetune_withoutlabel
 from parameters import EPOCHS, LEARN_RATE
-
-#加载模型
+# 加载模型
 sess = tf.Session()
 saver = tf.train.import_meta_graph("./model_o/model.ckpt.meta")
-saver.restore(sess,tf.train.latest_checkpoint("./model_o"))
+saver.restore(sess, tf.train.latest_checkpoint("./model_o"))
 
-#需要用到的参数，需要从池化层得到向量
+# 需要用到的参数，需要从池化层得到向量
 graph = tf.get_default_graph()
 nodes_node1 = graph.get_tensor_by_name("inputs/tree:0")
 children_node1 = graph.get_tensor_by_name("inputs/children:0")
 pool_out = graph.get_tensor_by_name("network/pooling/Max:0")
 
-#获得embedding索引表
+# 获得embedding索引表
 embeddings_index = {}
 fz = open("new 1.txt", 'r')
 line = "123"
@@ -32,21 +31,23 @@ fz.close()
 for i in range(len(listchar)):
     embeddings_index[listchar[i]] = i
 
-#将文件转为node和children两部分
-#########正文开始############
-dictt = {}
-file_string = "D:\\tbcnn_\\CVE-2018-4441.js"
-sample, size = get_tree(file_string)
-dictt[file_string] = sample
-nodes11,children1 = getData_finetune_withoutlabel(file_string,dictt,embeddings_index)
-out_put = sess.run(
-                [pool_out],
-                feed_dict={
-                    nodes_node1: nodes11,
-                    children_node1: children1,
-                }
-            )
-print(out_put)
+def fileString_convert_vector(file_path_name):
+    # 将文件转为node和children两部分
+    #########正文开始############
+    dictt = {}
+    # file_path_name = "D:\\tbcnn_\\CVE-2018-4441.js"
+    sample, size = get_tree(file_path_name)
+    dictt[file_path_name] = sample
+    nodes11,children1 = getData_finetune_withoutlabel(file_path_name,dictt,embeddings_index)
+    out_put = sess.run(
+                    [pool_out],
+                    feed_dict={
+                        nodes_node1: nodes11,
+                        children_node1: children1,
+                    }
+                )
+    return out_put
+    # print(out_put)
 
 
 
---
+++ test.py
@@ -0,0 +1,5 @@
+from restore_model import fileString_convert_vector
+
+file_path_name = "D:\\datasetforTBCCD-master\\jsc-CVE\\CVE-2017-6980.js"
+vector = fileString_convert_vector(file_path_name)
+print(vector)
