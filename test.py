04bf5bf7070c15a46490000acb5a7da7f48d8780
tjudoubi <2624593014@qq.com>
2020-08-17 22:48:58 +0800

    sd

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from restore_model import fileString_convert_vector
 
-file_path_name = "D:\\datasetforTBCCD-master\\jsc-CVE\\CVE-2017-6980.js"
+file_path_name = "D:\\tbcnn_\\CVE-2018-4441.js"
 vector = fileString_convert_vector(file_path_name)
+print(vector)
+
+file_path_name = "D:\\datasetforTBCCD-master\\jsc-CVE\\CVE-2017-6980.js"
+vector_ = fileString_convert_vector(file_path_name)
 print(vector)
