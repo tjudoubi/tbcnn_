8171a9a52108aa0ec0a430c34d4824ef60a49d91
tjudoubi <2624593014@qq.com>
2020-10-05 19:33:29 +0800

    sampleJS update

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from restore_model import fileString_convert_vector
 
-file_path_name = "D:\\tbcnn_\\CVE-2018-4441.js"
-vector = fileString_convert_vector(file_path_name)
-print(vector)
-
+# file_path_name = "D:\\tbcnn_\\CVE-2018-4441.js"
+# vector = fileString_convert_vector(file_path_name)
+# print(vector)
+#
 file_path_name = "D:\\datasetforTBCCD-master\\jsc-CVE\\CVE-2017-6980.js"
 vector_ = fileString_convert_vector(file_path_name)
-print(vector)
+print(vector_)
+
+file_path_name = "D:/datasetforTBCCD-master/Graduate_experiment-master/1.js"
+vector_ = fileString_convert_vector(file_path_name)
+print(vector_)
